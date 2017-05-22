d1cf4b8692beeb7631551632d2d29bad73b5c350
metalerk <rodriguezjluis0@gmail.com>
2017-05-22 13:10:12 -0500

    setup.py created

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='chatbase',
+    version='1.0',
+    packages=['', 'api', 'api.taps', 'api.batch', 'api.models', 'api.models.generic', 'api.models.facebook',
+              'api.generic', 'api.facebook', 'api.exceptions', 'docs', 'test', 'config', 'config.test'],
+    url='',
+    license='',
+    author='luis',
+    author_email='rodriguezjluis0@gmail.com',
+    description='Chatbase beta'
+)
